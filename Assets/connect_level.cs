9b4431c11e4584b5cb161f1c55970fdedfb19f05
ivanl1234 <leeholun1023@gmail.com>
2017-03-25 01:08:46 +0800

    alpha 0.1.1
    start menu connect (thinkpad to pc trans)

---
+++ Assets/connect_level.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class connect_level : MonoBehaviour
+{
+    public void level_connect(int level_connect)
+    {
+        SceneManager.LoadScene(level_connect);
+    }
+}
