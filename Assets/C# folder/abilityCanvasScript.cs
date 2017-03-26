4921b24326c4d4230852222675d90eaa6120c82f
ivanl1234 <leeholun1023@gmail.com>
2017-03-27 00:46:08 +0800

    alpha 1.1.1
    scenc connect& ui button ,ui text

---
+++ Assets/C# folder/abilityCanvasScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class abilityCanvasScript : MonoBehaviour {
+    public Text ATK;
+    public Text Hpmax;
+    public Text Spmax;
+    public Text Coin;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //Debug.Log(playerDataBase.Static.ATK);
+        ATK.text = "ATK+" + playerDataBase.Static.ATK;
+        Hpmax.text = "HPmax:" + playerDataBase.Static.MaxHP;
+        Spmax.text = "Spmax:" + playerDataBase.Static.MaxSP;
+        Coin.text = "Coin+" + playerDataBase.Static.COIN;
+    }
+}
