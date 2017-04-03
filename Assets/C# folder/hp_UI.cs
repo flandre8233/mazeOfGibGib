bd7baf47c8a94a1546797c179e7eecf6d630d768
flandre8233 <flandre8233@gmail.com>
2017-04-04 02:10:14 +0800

    alpha 1.42
    shop system :coin add

---
+++ Assets/C# folder/hp_UI.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class hp_UI : MonoBehaviour {
+    public Text coin_show;
+
+	void Update () {
+        //coin_show = playerDataBase.Static.COIN;
+	}
+}
