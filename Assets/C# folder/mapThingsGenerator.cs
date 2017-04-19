3437ee1642bcde69235846ccdba73e39d7af854a
gibson8233 <gibson8233@hotmail.com>
2017-04-20 04:09:01 +0800

    alpha 2.1.1

--- Assets/C# folder/mapThingsGenerator.cs
+++ Assets/C# folder/mapThingsGenerator.cs
@@ -185,6 +185,11 @@ public class mapThingsGenerator : MonoBehaviour {
     }
 
     public void SerializePlayerPositionToSpawnPoint() {
+        if (playerDataBase.Static.currentFloor % 5 == 0) { //到5,10,15,20......關卡  休息關重生點
+            Vector3 targetV3 = new Vector3(1,1 -1);
+            player.transform.position = targetV3;
+            return;
+        }
         /*
         if (mapTerrainGenerator.Static.thisLevelAllFloor.Count != 0) {
             foreach (var item in mapTerrainGenerator.Static.thisLevelAllFloor) {
